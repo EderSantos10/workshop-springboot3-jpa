098815bf47a504244825dd37eed7a422eea5acf3
Eder Vinicius <eder.e10@hotmail.com>
2024-02-07 16:58:35 -0300

    Service layer, component registration

---
+++ src/main/java/com/edervini/aulaspring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.edervini.aulaspring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edervini.aulaspring.entities.User;
+import com.edervini.aulaspring.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();		
+	}
+	
+}
