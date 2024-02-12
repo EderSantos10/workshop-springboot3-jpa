5c06db4a769d8fea9f310ba504f34a335a2c46cf
Eder Vinicius <eder.e10@hotmail.com>
2024-02-12 10:37:54 -0300

    Exception handling - findById

---
+++ src/main/java/com/edervini/aulaspring/resources/excepstions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.edervini.aulaspring.resources.excepstions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.edervini.aulaspring.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		
+		String error  = "Resourse not found!";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
--- src/main/java/com/edervini/aulaspring/services/UserService.java
+++ src/main/java/com/edervini/aulaspring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.edervini.aulaspring.entities.User;
 import com.edervini.aulaspring.repositories.UserRepository;
+import com.edervini.aulaspring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();		
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));		
 	}
 	
 	public User insert(User obj) {
